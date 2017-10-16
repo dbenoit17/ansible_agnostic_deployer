0e9b48057caa3695b8bda9750435265e259f4098
David Benoit <dbenoit@redhat.com>
2017-10-16 16:11:14 +0000

    updates

--- ansible/tools/python_templates/rhcls-modernize-devops.py
+++ ansible/tools/python_templates/rhcls-modernize-devops.py
@@ -16,20 +16,28 @@ master = ravello.Vm(name="1master", tag="master", boot_disk_size_GB=80,
                      num_cpus=4, mem_size=16)
 master.add_hard_drive(name='docker_pool', size=10)
 
-node1 = ravello.Vm(name="2node1", tag="node1", boot_disk_size_GB=60, 
+node1 = ravello.Vm(name="2node1", tag="node01", boot_disk_size_GB=60, 
                      ip="192.168.0.11", mac="2c:c2:60:43:e9:16",
                      num_cpus=4, mem_size=12)
 node1.add_hard_drive(name='docker', size=10)
 
-node2 = ravello.Vm(name="3node2", tag="node2", boot_disk_size_GB=60, 
+node2 = ravello.Vm(name="3node2", tag="node02", boot_disk_size_GB=60, 
                      ip="192.168.0.12", mac="2c:c2:60:67:ea:14",
                      num_cpus=4, mem_size=12)
 node2.add_hard_drive(name='docker', size=10)
 
-node3 = ravello.Vm(name="4node3", tag="node3", boot_disk_size_GB=60, 
+node3 = ravello.Vm(name="4node3", tag="node03", boot_disk_size_GB=60, 
                      ip="192.168.0.13", mac="2c:c2:60:7b:f7:e4",
                      num_cpus=4, mem_size=12)
 node3.add_hard_drive(name='docker', size=10)
 
-template = ravello.Template(workstation, master, node1, node2, node3)
+cloudforms = ravello.Vm(name="5cloudforms", tag="cfme", boot_disk_size_GB=60, 
+                     ip="192.168.0.50", mac="2c:c2:60:0c:e9:65", 
+                     boot_image='disk-image-cfme-vsphere-5.8.1.5-1.x86_64.qcow',
+                     num_cpus=4, mem_size=12)
+cloudforms.add_hard_drive(name='vol', size=20)
+cloudforms.add_service(name='http', external=True, port_range=80, protocol='TCP')
+cloudforms.add_service(name='https', external=True, port_range=443, protocol='TCP')
+
+template = ravello.Template(workstation, master, node1, node2, node3, cloudforms)
 print template.to_yaml()
